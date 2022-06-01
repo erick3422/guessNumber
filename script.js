8d7f7c0d7d6455c317ea9054faa2bcc940bcca2d
erick3422 <erickloco882@yahoo.com>
2022-05-31 17:11:53 -0700

    Added Condition For High Number Added Condition For Low Number Added Condition For No Number

--- script.js
+++ script.js
@@ -17,11 +17,23 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value)
 	console.log(guess, typeof guess)
 
 	if (!guess) {
 		document.querySelector('.message').textContent = 'No number ⚠️⚠️⚠️⚠️⚠️⚠️'
-	}
+	} else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'You win!🏆🏆🏆'
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Check Your Number buddy Too High 📈📈📈'
+    } else if ( guess < secretNumber){
+        document.querySelector('.message').textContent = 'Check Your Number buddy Too Low 📉📉📉'
+    }
 })
+
+console.log(secretNumber);
